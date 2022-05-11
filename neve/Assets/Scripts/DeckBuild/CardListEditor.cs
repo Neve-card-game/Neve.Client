a27b917f8be7f518495f2bd515581673914e5fc8
TheO1dF <51126474+TheO1dF@users.noreply.github.com>
2022-05-12 03:51:43 +0800

    实现主界面、卡牌收藏

---
+++ neve/Assets/Scripts/DeckBuild/CardListEditor.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class CardListEditor : MonoBehaviour, IPointerClickHandler
+{
+    public delegate void OnClickThisCard(PointerEventData eventData);
+    public static event OnClickThisCard CardOnClick;
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        Debug.Log("移除卡牌：" + this.name);
+        CardOnClick(eventData);
+    }
+}
---
+++ neve/Assets/Scripts/MainMenu/FULLScreen.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FULLScreen : MonoBehaviour
+{
+    Toggle toggle;
+    private void Start()
+    {
+         toggle= FindObjectOfType<Toggle>();
+    }
+
+   
+   private void SetUnFullScreen()
+    {
+        Screen.fullScreen = false;
+    }
+
+    private  void SetFullScreen()
+    {
+        Screen.SetResolution(1920, 1080, true);
+        Screen.fullScreen = true;
+    }
+
+    public void SetScreen()
+    {
+        if (toggle.isOn)
+        {
+            Debug.Log("开启全屏");
+            SetFullScreen();
+        }
+        else
+        {
+            Debug.Log("关闭全屏");
+            SetUnFullScreen();
+        }
+    }
+}
